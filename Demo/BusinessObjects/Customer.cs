38db98266865e4714fed72c9e02939dc9c916162
HzKharel <43970402+HzKharel@users.noreply.github.com>
2018-10-08 22:52:39 +0100

    initial commit
    The base GUI is completed, started on the customer class.

---
+++ Demo/BusinessObjects/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessObjects
+{
+    public class Customer
+    {
+        private int _customerID;
+        private int _customerTelephone;
+        private string _customerName;
+        private string _customerLastName;
+        private string _customerEmail;
+        private string _customerSkype;
+
+
+        public int ID
+        {
+            get
+            {
+                return _customerID;
+            }
+            set
+            {
+                _customerID = value;
+            }
+        }
+
+        public int Telephone
+        {
+            get
+            {
+                return _customerTelephone;
+            }
+            set
+            {
+                _customerTelephone = value;
+            }
+        }
+        
+        public string Name
+        {
+            get 
+        }
+    }
+}
---
+++ Demo/Demo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BusinessObjects;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MailingList store = new MailingList();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Find_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Find_btn.Visibility = Visibility.Collapsed;
+            Find_Grid.Visibility = Visibility.Visible;
+        }
+
+        private void AddCustomer_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
